83cad78db55f5232d0912b2fc2df1f26c9e47fb3
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-03-26 15:52:47 +0900

    Update

--- skill/account-link.js
+++ skill/account-link.js
@@ -2,25 +2,25 @@
 
 const debug = require("debug")("skill");
 const memory = require("memory-cache");
-const user_db = require("../service/user-db");
 
 module.exports = (line_client, event) => {
     debug(`Got accountLink event.`);
     debug(event);
 
-    /*
-    // Retrieve cp user id.
-    let cp_user_id = memory.get(event.accountLink.nonce).cp_user_id;
-
-    // Save mapping of cp user id and line user id.
-    return user_db.save({
-        user_id: cp_user_id,
-        line_user_id: line_user_id
-    }).then((response) => {
+    if (event.link.result !== "ok"){
+        // Account link failed.
         return line_client.replyMessage(event.replyToken, {
             type: "text",
-            text: "アカウントがリンクされました"
-        })
+            text: "リンクに失敗しました。"
+        });
+    }
+
+    return line_client.replyMessage(event.replyToken, [{
+        type: "sticker",
+        packageId: "2",
+        stickerId: "144"
+    },{
+        type: "text",
+        text: `リンク完了です。ここでnonce: ${event.link.nonce} をキーにして保存してあったCPのUser Id: ${memory.get(event.link.nonce).cp_user_id} を取り出し、LINEのUser Id: ${event.source.userId} と紐づけてデータベースに保存します。`
     })
-    */
 }
